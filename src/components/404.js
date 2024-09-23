c178b8926344f7e4990092fadd09a607ed919532
only1stumpy <rei228322@gmail.com>
2024-09-24 01:35:37 +0300

    fixed color 404

--- src/components/404.js
+++ src/components/404.js
@@ -4,7 +4,7 @@ export default function NotFound(){
     return(
         <div className="flex flex-col w-screen h-screen text-[100px] text-white justify-center items-center font-roboto">
             404 Not Found
-            <a href="/">Go Home</a>
+            <a href="/" className="text-yellow-300">Go Home</a>
         </div>
     )
 }
