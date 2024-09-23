9114a9388281d922d06fab85326bf3a223da38b0
only1stumpy <rei228322@gmail.com>
2024-09-24 01:30:38 +0300

    404

--- src/components/404.js
+++ src/components/404.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function NotFound(){
     return(
-        <div className="flex w-screen h-screen text-[200px] text-white justify-center items-center font-roboto">
+        <div className="flex flex-col w-screen h-screen text-[100px] text-white justify-center items-center font-roboto">
             404 Not Found
             <a href="/">Go Home</a>
         </div>
